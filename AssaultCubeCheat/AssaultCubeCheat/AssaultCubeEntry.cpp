bea80683895942b45904cb14e7b74e0ca77dbf0d
VitaAetaerna <muhleon18@gmail.com>
2023-11-06 09:14:52 +0100

    More features

--- AssaultCubeCheat/AssaultCubeCheat/AssaultCubeEntry.cpp
+++ AssaultCubeCheat/AssaultCubeCheat/AssaultCubeEntry.cpp
@@ -9,11 +9,18 @@ DWORD localPlayerPointer;
 //Values
 int healthToSet = 999;
 int currentHealth;
+
+int ammoToSet = 100;
+int currentAmmo;
+
+int speedToAdd = 1;  
+int currentSpeed;
 int main(){
 	
 	//Get Process ID and Base Address
 	pID = getProcessID(L"ac_client.exe");
 	baseModule = GetModuleBaseAddress(pID, L"ac_client.exe");
+	std::cout << "Process name: ac_client.exe" << std::endl;
 
 	std::cout << pID << std::endl;
 	std::cout << std::hex << baseModule << std::endl;
@@ -25,6 +32,7 @@ int main(){
 	std::cout << "Local Player Addr: " << std::hex << localPlayerPointer << std::endl;
 
 	while (true) {
+	//Health
 		//Read Process memory for current Health | storing it in currentHealth
 		ReadProcessMemory(handle, (LPVOID)(localPlayerPointer + 0xEC), &currentHealth, sizeof(currentHealth), nullptr);
 		if (currentHealth < 100) {
@@ -32,6 +40,30 @@ int main(){
 			WriteProcessMemory(handle, (LPVOID)(localPlayerPointer + 0xEC), &healthToSet, sizeof(&healthToSet), nullptr);
 			std::cout << "Health changed to " << healthToSet << std::endl;
 		}
+
+	//Ammo Gun
+		ReadProcessMemory(handle, (LPCVOID)(localPlayerPointer + AMMOPOINTER), &currentAmmo, sizeof(currentAmmo), nullptr);
+		if (currentAmmo < 10) {
+			WriteProcessMemory(handle, (LPVOID)(localPlayerPointer + AMMOPOINTER), &ammoToSet, sizeof(ammoToSet), nullptr);
+			std::cout << "Ammo changed to " << ammoToSet << std::endl;
+		}
+
+	//Bunnyhop or Speed change
+		ReadProcessMemory(handle, (LPCVOID)(localPlayerPointer + SPEEDPOINTER), &currentSpeed, sizeof(currentSpeed), nullptr);
+		if (GetKeyState('F') && 0x8000){
+			WriteProcessMemory(handle, (LPVOID)(localPlayerPointer + SPEEDPOINTER), &currentSpeed+speedToAdd, sizeof(speedToAdd), nullptr);
+		}
+
+	// ?Changing Damage of Weapon?
+
+	// ESP / Wallhack
+
+	// fly?
+
+	//teleport?
+
+
+
 	}
 
 }
